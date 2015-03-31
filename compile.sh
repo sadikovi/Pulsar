83a29085843fa82fe446faa1154b0350432830c6
sadikovi <ivan.sadikov@lincolnuni.ac.nz>
2015-04-01 03:15:08 +1300

    main page is added, pulsar 1.0 files are deleted

---
+++ compile.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+lessc static/stylesheets/font.less > static/stylesheets/font.css
+lessc static/stylesheets/base.less > static/stylesheets/base.css
+lessc static/stylesheets/grid.less > static/stylesheets/grid.css
+lessc static/stylesheets/panel.less > static/stylesheets/panel.css
+lessc static/stylesheets/button.less > static/stylesheets/button.css
+lessc static/stylesheets/text.less > static/stylesheets/text.css
+lessc static/stylesheets/utility.less > static/stylesheets/utility.css
